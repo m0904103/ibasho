f1c1cb66b2aa70ce4c541ee433df274f5fd16e9f
m0904103 <manpower0832@gmail.com>
2026-07-15 10:07:21 +0800

    Adjust timing of Sumibiraki slide to 07:42 (0.806)

--- main.js
+++ main.js
@@ -95,7 +95,7 @@ document.addEventListener('DOMContentLoaded', () => {
     { percent: 0.710, title: '陪伴與留白' },
     { percent: 0.747, title: '孤食' },
     { percent: 0.783, title: '社會處方箋' },
-    { percent: 0.815, title: '住み開き' },
+    { percent: 0.806, title: '住み開き' },
     { percent: 0.841, title: '出番 (出場舞台)' },
     { percent: 0.864, title: '恩送 Pay It Forward' },
     { percent: 0.884, title: '軟著陸' },
